ddeb5a15a7dcd0f42eaf8acc0cfec322de549367
Tarun <tarunkeshukumar@gmail.com>
2023-10-01 16:29:56 +0530

    initial commit

---
+++ snippets/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Snippet
+# Register your models here.
+
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ("highlighted",)
+
+admin.site.register(Snippet, SnippetAdmin)
